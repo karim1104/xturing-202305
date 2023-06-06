02afc279331259a52d960a2246a2f87c79b81dc5
karim1104 <karim1104@gmail.com>
2023-06-06 05:23:48 +0400

    Update bloom_engine.py

--- src/xturing/engines/bloom_engine.py
+++ src/xturing/engines/bloom_engine.py
@@ -8,7 +8,7 @@ class BloomEngine(CausalEngine):
     config_name: str = "bloom_engine"
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
-        super().__init__(model_name="bigscience/bloomz-3b", weights_path=weights_path)
+        super().__init__(model_name="karim1104/final", weights_path=weights_path)
 
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
@@ -19,7 +19,7 @@ class BloomLoraEngine(CausalLoraEngine):
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
         super().__init__(
-            model_name="bigscience/bloomz-3b",
+            model_name="karim1104/final",
             weights_path=weights_path,
             target_modules=["query_key_value"],
         )
@@ -33,7 +33,7 @@ class BloomInt8Engine(CausalEngine):
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
         super().__init__(
-            model_name="bigscience/bloomz-3b",
+            model_name="karim1104/final",
             weights_path=weights_path,
             load_8bit=True,
         )
@@ -46,7 +46,7 @@ class BloomLoraInt8Engine(CausalLoraEngine):
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
         super().__init__(
-            model_name="bigscience/bloomz-3b",
+            model_name="karim1104/final",
             weights_path=weights_path,
             load_8bit=True,
             target_modules=["query_key_value"],
